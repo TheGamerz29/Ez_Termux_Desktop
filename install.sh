acc6cec8e525ef8e40fb942ad7fdcca80169b66f
TheGamerz29 <66721586+TheGamerz29@users.noreply.github.com>
2020-10-07 11:48:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 ## Code from adi1090! Thx!. Without him, i can't create this!
+apt install -y figlet screenfetch neofetch
+clear
+figlet -c       EZ_TERMUX_DESKTOP
+figlet -c         By TheGamerz
+figlet -c thx to adi1090x for the toturial!
 echo "This will install a desktop environment for Termux."
 echo "It will take around 900mb-1gb of your space to install."
 read -p "Continue? (y/n) " answer
 case ${answer:0:1} in
     y|Y )
-        echo "Downloading required package..."
-        pkg update && pkg upgrade && pkg install -y x11-repo && pkg install -y tigervnc openbox obconf xorg-xsetroot xcompmgr xterm polybar st libnl zsh geany pcmanfm rofi feh neofetch htop vim elinks mutt git wget curl xfce4-settings
-        echo "done."
+        echo "downloading required files..."
+        pkg update && pkg upgrade && pkg install -y x11-repo && pkg install -y tigervnc openbox obconf netsurf xorg-xsetroot xcompmgr xterm polybar st libnl zsh geany pcmanfm rofi feh neofetch htop vim elinks mutt git wget curl xfce4-settings
+        figlet -c DONE.
         clear
         echo "Configuring..."
         cd $HOME && git clone https://github.com/adi1090x/termux-desktop
@@ -17,11 +22,17 @@ case ${answer:0:1} in
         export DISPLAY=":1"
         vncserver -kill :1
         clear
-        echo "You can now start the desktop by typing:"
+        figlet -c  Congratulations!
+        echo "  Ez_Termux_Desktop"
+        echo " Have been installed successfully on this device"
+        echo.
+        echo "You can now access them by typing:"
         echo "vncserver"
-        echo "To stop it, type:"
-        echo "vncserver -kill {display number}"
-        echo "DONE!"
+        echo.
+        echo "To stop the server, type:"
+        echo "vncserver -kill {Display Number}"
+        echo.
+        screenfetch
     ;;
     * )
         echo "Abort..."
