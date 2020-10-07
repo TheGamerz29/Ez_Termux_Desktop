6c39eacc9e6fc71ff45e25e96cd1a2652bb94fa8
TheGamerz29 <66721586+TheGamerz29@users.noreply.github.com>
2020-10-07 11:52:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 ## Code from adi1090! Thx!. Without him, i can't create this!
-apt install -y figlet screenfetch neofetch
+apt install -y figlet screenfetch
+screenfetch
 clear
 figlet -c       EZ_TERMUX_DESKTOP
 figlet -c         By TheGamerz
 figlet -c thx to adi1090x for the toturial!
 echo "This will install a desktop environment for Termux."
 echo "It will take around 900mb-1gb of your space to install."
+echo.
 read -p "Continue? (y/n) " answer
 case ${answer:0:1} in
     y|Y )
+        screenfetch
+        clear
         echo "downloading required files..."
         pkg update && pkg upgrade && pkg install -y x11-repo && pkg install -y tigervnc openbox obconf netsurf xorg-xsetroot xcompmgr xterm polybar st libnl zsh geany pcmanfm rofi feh neofetch htop vim elinks mutt git wget curl xfce4-settings
         figlet -c DONE.
@@ -21,6 +25,7 @@ case ${answer:0:1} in
         vncserver -localhost
         export DISPLAY=":1"
         vncserver -kill :1
+        screenfetch
         clear
         figlet -c  Congratulations!
         echo "  Ez_Termux_Desktop"
@@ -32,9 +37,11 @@ case ${answer:0:1} in
         echo "To stop the server, type:"
         echo "vncserver -kill {Display Number}"
         echo.
-        screenfetch
     ;;
     * )
+        screenfetch
+        clear
+        figlet -c WHY DID YOU DO THIS TO ME?!??!
         echo "Abort..."
     ;;
 esac
